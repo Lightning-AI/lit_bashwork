f0fdf7e696819fe1b7765afe2f34670974d0aadf
Robert S Lee <sangkyulee@gmail.com>
2022-07-15 14:26:37 -0400

    setup test 	modified:   tests/test_lit_bashwork_component.py

--- tests/test_lit_bashwork_component.py
+++ tests/test_lit_bashwork_component.py
@@ -4,10 +4,10 @@ To test a lightning component:
 1. Init the component.
 2. call .run()
 """
-from lit_bashwork.component import TemplateComponent
+from lit_bashwork.component import LitBashWork
 
 
 def test_placeholder_component():
-    messenger = TemplateComponent()
-    messenger.run()
-    assert messenger.value == 1
+    messenger = LitBashWork()
+    messenger.run("ls README.md")
+    assert messenger.stdout[0] == "README.md"
