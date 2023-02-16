1ea8044c1f82084fd094f01b7111d7c7fe84e85c
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-17 03:03:57 +0900

    update precommit (#3)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import lightning.app as la
+
 from lit_bashwork import LitBashWork
 
-import lightning.app as la
 
 class LitApp(la.LightningFlow):
     def __init__(self) -> None:
@@ -10,21 +11,22 @@ class LitApp(la.LightningFlow):
         self.w_p1c0 = LitBashWork(parallel=True)
         self.w_p1c1 = LitBashWork(parallel=True, cache_calls=False)
 
-    def run_test(self,w):
-      w.run("sleep 20")
-      if w.work_is_free():
-        w.run("sleep 5",timeout=0)
-      if w.work_is_free():
-        w.run("sleep 5",timeout=1)
-      if w.work_is_free():
-        w.run("sleep 5",timeout=6)
-      if w.work_is_free():
-        print(f"work is free.  called {w.work_calls_len()}")
-        
+    def run_test(self, w):
+        w.run("sleep 20")
+        if w.work_is_free():
+            w.run("sleep 5", timeout=0)
+        if w.work_is_free():
+            w.run("sleep 5", timeout=1)
+        if w.work_is_free():
+            w.run("sleep 5", timeout=6)
+        if w.work_is_free():
+            print(f"work is free.  called {w.work_calls_len()}")
+
     def run(self):
-      self.run_test(self.w_p0c0)
-      self.run_test(self.w_p0c1)
-      self.run_test(self.w_p1c0)
-      self.run_test(self.w_p1c1)
+        self.run_test(self.w_p0c0)
+        self.run_test(self.w_p0c1)
+        self.run_test(self.w_p1c0)
+        self.run_test(self.w_p1c1)
+
 
 app = la.LightningApp(LitApp())
